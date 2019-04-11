b14db9ce289f075a141545a2baf1c97bd5dcdddd
Gavin Cornwell <gcornwell@gmail.com>
2019-04-11 16:19:37 +0100

    Re-factored to use promises/async/await.

--- cleaner.js
+++ cleaner.js
@@ -4,139 +4,124 @@ const AWS = require("aws-sdk");
 const codebuild = new AWS.CodeBuild();
 const cloudwatchlogs = new AWS.CloudWatchLogs();
 
-exports.handler = (event, context, callback) => {
-    console.log("Received event: " + JSON.stringify(event, null, 2));
-    
-    // build a map of codebuild project names
-    buildListOfProjectNames([], null, function(error, projectNames) {
-        if (error) {
-            console.log(error, error.stack);
-            callback(error);
-        } else {
-            console.log("Found " + projectNames.length + " projects.");
-            console.log("Project names: " + projectNames);
-            
-            buildListOfGroupNames([], null, function(error, groupNames) {
-                if (error) {
-                    console.log(error, error.stack);
-                    callback(error);
-                } else {
-                    console.log("Found " + groupNames.length + " log groups.");
-                    console.log("Log group names: " + groupNames);
-                    
-                    var groupsProcessed = 0;
-                    var groupsIgnored = 0;
-                    var groupsDeleted = 0;
-                    var groupsFailed = 0;
-                    
-                    // iterate through log groups and look for associated codebuild project
-                    for (let i = 0; i < groupNames.length; i++) {
-                     
-                        // strip prefix from group name
-                        let projectName = groupNames[i].substring(15);
-                        
-                        // look for name in list of functions
-                        var projectExists = projectNames.includes(projectName);
-                        
-                        // if missing, delete the log group
-                        if (projectExists) {
-                            groupsIgnored++;
-                            groupsProcessed++;
-                            console.log("Ignoring group " + groupNames[i] + " as the CodeBuild project exists");
-                        } else {
-                            console.log("Deleting group " + groupNames[i] + " as the CodeBuild project no longer exists...");
-                            
-                            var deleteGroupParams = {
-                                logGroupName: groupNames[i]
-                            };
-                            cloudwatchlogs.deleteLogGroup(deleteGroupParams, function(error) {
-                                groupsProcessed++;
-                                
-                                if (error) {
-                                    groupsFailed++;
-                                } else {
-                                    groupsDeleted++;
-                                }
-                                
-                                // if all groups have been processed, call the main callback with results
-                                if (groupsProcessed == groupNames.length) {
-                                    let result = {
-                                        groupsProcessed: groupsProcessed,
-                                        groupsIgnored: groupsIgnored,
-                                        groupsDeleted: groupsDeleted,
-                                        groupsFailed: groupsFailed
-                                    };
-                                    console.log("Returning result: " + JSON.stringify(result, null, 2));
-                                    callback(null, result);
-                                }
-                            });
-                        }
-                    }
-                }
-            });
-        }
-    });
+exports.handler = async (event) => {
+  console.log("Received event: " + JSON.stringify(event, null, 2));
+
+  try {
+    // retrieve lists is parallel
+    let results = await Promise.all([buildListOfProjectNames(), buildListOfGroupNames()]);
+    let projectNames = results[0];
+    let groupNames = results[1];
+    console.log("Found " + projectNames.length + " functions.");
+    console.log("Found " + groupNames.length + " log groups.");
+
+    // do the processing and return result
+    var processingResults = await processLogGroups(projectNames, groupNames);
+    console.log("Returning result: " + JSON.stringify(processingResults, null, 2));
+    return processingResults;
+  } catch (e) {
+    throw new Error("Failed to clean log groups: " + e.message);
+  }
 };
 
-var buildListOfProjectNames = function(projectNames, nextToken, projectNamesCallback) {
-    
-    let listProjectsParams = {};
-    
-    if (nextToken) {
-        listProjectsParams.nextToken = nextToken;
-    }
-    
-    // console.log("Calling listFunctions with params: " + JSON.stringify(listFunctionsParams, null, 2));
-    codebuild.listProjects(listProjectsParams, function(error, data) {
-        if (error) {
-            console.log(error, error.stack);
-            projectNamesCallback(error);
-        } else {
-            // console.log("Found " + data.projects.length + " projects.");
-            // console.log("listProjects result: " + JSON.stringify(data, null, 2));
-            
-            for (var i = 0; i < data.projects.length; i++) {
-                projectNames.push(data.projects[i]);
-            }
-            
-            if (data.nextToken) {
-                buildListOfProjectNames(projectNames, data.nextToken, projectNamesCallback);
-            } else {
-                projectNamesCallback(null, projectNames);
-            }
-        }
-    });
+var buildListOfProjectNames = async () => {
+  try {
+    let projectNames = [];
+    let nextToken = null;
+    let params = {};
+
+    do {
+      if (nextToken !== null) {
+        params.nextToken = nextToken;
+      }
+      let data = await codebuild.listProjects(params).promise();
+      //console.log("listProjects response: " + JSON.stringify(data, null, 2));
+      for (var i = 0; i < data.projects.length; i++) {
+        projectNames.push(data.projects[i]);
+      }
+      nextToken = data.nextToken;
+    } while (nextToken !== null && nextToken !== undefined);
+
+    return projectNames;
+  } catch (e) {
+    throw e;
+  }
 };
 
-var buildListOfGroupNames = function(groupNames, nextToken, logGroupNamesCallback) {
-    
-    let describeLogGroupsParams = {
-        logGroupNamePrefix: "/aws/codebuild/",
-        limit: 25
+var buildListOfGroupNames = async () => {
+  try {
+    let groupNames = [];
+
+    let nextToken = null;
+    let params = {
+      logGroupNamePrefix: "/aws/codebuild/",
+      limit: 25
     };
-    
-    if (nextToken) {
-        describeLogGroupsParams.nextToken = nextToken;
-    }
-    
-    // console.log("Calling describeLogGroups with params: " + JSON.stringify(describeLogGroupsParams, null, 2));
-    cloudwatchlogs.describeLogGroups(describeLogGroupsParams, function(error, data) {
-        if (error) {
-            console.log(error, error.stack);
-            logGroupNamesCallback(error);
-        } else {
-            // console.log("Found " + data.logGroups.length + " log groups.");
-            // console.log("describeLogGroups result: " + JSON.stringify(data, null, 2));
-            
-            for (var i = 0; i < data.logGroups.length; i++) {
-                groupNames.push(data.logGroups[i].logGroupName);
-            }
-            
-            if (data.nextToken) {
-                buildListOfGroupNames(groupNames, data.nextToken, logGroupNamesCallback);
-            } else {
-                logGroupNamesCallback(null, groupNames);
-            }
+
+    do {
+      if (nextToken !== null) {
+        params.nextToken = nextToken;
+      }
+      let data = await cloudwatchlogs.describeLogGroups(params).promise();
+      //console.log("describeLogGroups response: " + JSON.stringify(data, null, 2));
+      for (var i = 0; i < data.logGroups.length; i++) {
+        groupNames.push(data.logGroups[i].logGroupName);
+      }
+      nextToken = data.nextToken;
+    } while (nextToken !== null && nextToken !== undefined);
+
+    return groupNames;
+  } catch (e) {
+    throw e;
+  }
+};
+
+var processLogGroups = async (projectNames, groupNames) => {
+  try {
+    let groupsProcessed = 0;
+    let groupsIgnored = 0;
+    let groupsDeleted = 0;
+    let groupsFailed = 0;
+
+    // iterate through log groups and look for associated CodeBuild project
+    for (let i = 0; i < groupNames.length; i++) {
+      groupsProcessed++;
+      let groupName = groupNames[i];
+
+      // strip prefix from group name
+      let projectName = groupName.substring(15);
+
+      // look for name in list of projects
+      var projectExists = projectNames.includes(projectName);
+
+      // if project is missing, delete the log group
+      if (projectExists) {
+        groupsIgnored++;
+        console.log("Ignoring group " + groupName + " as the CodeBuild project exists");
+      } else {
+        console.log("Deleting group " + groupName + " as the CodeBuild project no longer exists...");
+
+        var deleteGroupParams = {
+          logGroupName: groupName
+        };
+        try {
+          await cloudwatchlogs.deleteLogGroup(deleteGroupParams).promise();
+          groupsDeleted++;
+        } catch (deleteError) {
+          console.log("Failed to delete group " + groupName);
+          groupsFailed++;
         }
-    });
+      }
+    }
+
+    // build and return result object
+    return {
+      groupsProcessed: groupsProcessed,
+      groupsIgnored: groupsIgnored,
+      groupsDeleted: groupsDeleted,
+      groupsFailed: groupsFailed
+    };
+  } catch (e) {
+    throw e;
+  }
 };
